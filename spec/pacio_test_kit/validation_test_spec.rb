3acec438a13d41473525762f51e906526231283e
Vanessa Fotso <vfotso@mitre.org>
2024-07-07 23:14:55 -0400

    Added validation test unit tests
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

---
+++ spec/pacio_test_kit/validation_test_spec.rb
@@ -0,0 +1,82 @@
+RSpec.describe PacioTestKit::ValidationTest do
+  let(:runnable) { Inferno::Repositories::Tests.new.find('pacio_resource_validation') }
+  let(:session_data_repo) { Inferno::Repositories::SessionData.new }
+  let(:results_repo) { Inferno::Repositories::Results.new }
+  let(:test_session) { repo_create(:test_session, test_suite_id: 'pacio_pfe_server') }
+  let(:url) { 'https://example/r4' }
+  let(:resource_type) { 'Observation' }
+  let(:resource_id) { '123' }
+  let(:profile) { 'PFESingleObservation' }
+  let(:observation) do
+    JSON.parse(
+      File.read(File.join(__dir__, '..', 'fixtures', 'pfe_single_observation.json'))
+    )
+  end
+
+  def build_read_request(body: nil, status: 200, headers: nil)
+    repo_create(
+      :request,
+      direction: 'outgoing',
+      url: "#{url}/#{resource_type}/#{resource_id}",
+      test_session_id: test_session.id,
+      response_body: body.is_a?(Hash) ? body.to_json : body,
+      status:,
+      headers:
+    )
+  end
+
+  def mock_server(body: nil, status: 200, valid_resource: false)
+    request = build_read_request(body:, status:)
+    allow_any_instance_of(runnable).to receive(:requests).and_return([request])
+    allow_any_instance_of(runnable).to receive(:resource_is_valid?).and_return(valid_resource)
+    messages = [{ type: 'error', message: 'resource not conformant' }]
+    allow_any_instance_of(runnable).to receive(:messages).and_return(messages) unless valid_resource
+  end
+
+  def run(runnable, inputs = {})
+    test_run_params = { test_session_id: test_session.id }.merge(runnable.reference_hash)
+    test_run = Inferno::Repositories::TestRuns.new.create(test_run_params)
+    inputs.each do |name, value|
+      session_data_repo.save(
+        test_session_id: test_session.id,
+        name:,
+        value:,
+        type: runnable.config.input_type(name)
+      )
+    end
+
+    Inferno::TestRunner.new(test_session:, test_run:).run(runnable)
+  end
+
+  before do
+    allow_any_instance_of(runnable).to receive(:resource_type).and_return(resource_type)
+    allow_any_instance_of(runnable).to receive(:tag).and_return(profile)
+  end
+
+  it 'passes when at least one previous read or search request is successful and returns conformant resource' do
+    mock_server(body: observation, valid_resource: true)
+    result = run(runnable)
+    expect(result.result).to eq('pass')
+  end
+
+  it 'skips when no read or search requests were made in presvious tests' do
+    result = run(runnable)
+    expect(result.result).to eq('skip')
+    expect(result.result_message).to match(/No #{profile} resource read or search request was made in previous tests/)
+  end
+
+  it 'skips when all read or search requests were unsuccessful' do
+    mock_server(status: 401, valid_resource: true)
+    result = run(runnable)
+    expect(result.result).to eq('skip')
+    expect(result.result_message).to match(/read or search requests were unsuccessful/)
+  end
+
+  it 'fails if at least one of the returned resources is not conformant to the given profile' do
+    mock_server(body: { resourceType: resource_type })
+    result = run(runnable)
+
+    expect(result.result).to eq('fail')
+    expect(result.result_message).to match(/resources returned in previous tests do not conform to the profile/)
+  end
+end
