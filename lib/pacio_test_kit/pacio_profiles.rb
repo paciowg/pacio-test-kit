b44a8a033dbc80fa28dfb63f1ea8530f7f19e7cf
Vanessa Fotso <vfotso@mitre.org>
2024-07-07 23:16:21 -0400

    Added profiles constant
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

---
+++ lib/pacio_test_kit/pacio_profiles.rb
@@ -0,0 +1,18 @@
+module PacioTestKit
+  module PacioProfiles
+    PACIO_PROFILES =
+      {
+        'PFEClinicalTestObservation' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-observation-clinicaltest',
+        'PFECollection' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-collection',
+        'PFEConditionEncounterDiagnosis' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-condition-encounter-diagnosis',
+        'PFEConditionProblemsHealthConcerns' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-condition-problems-health-concerns',
+        'PFEUseOfDevice' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-use-of-device',
+        'PFENarrativeHistoryOfStatus' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-narrative-history-of-status',
+        'PFEDiagnosticReportNoteExchange' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-diagnostic-report-note-exchange',
+        'PFEGoal' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-goal',
+        'PFENutritionOrder' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-nutrition-order',
+        'PFEServiceRequest' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-service-request',
+        'PFESingleObservation' => 'http://hl7.org/fhir/us/pacio-pfe/StructureDefinition/pfe-observation-single'
+      }.freeze
+  end
+end
