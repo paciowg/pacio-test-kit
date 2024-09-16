848a7fef91166b41b707d9a72264056cb5e96836
Vanessa Fotso <vfotso@mitre.org>
2024-09-15 21:58:52 -0400

    Capability statement tests
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/pacio_test_kit/pacio_adi_server_suite.rb
+++ lib/pacio_test_kit/pacio_adi_server_suite.rb
@@ -1,5 +1,10 @@
+require_relative 'pacio_profiles'
+require_relative 'custom_groups/capability_statement_group'
+
 module PacioTestKit
   class PacioADIServerSuite < Inferno::TestSuite
+    include PacioTestKit::PacioProfiles
+
     id :pacio_adi_server
     title 'PACIO ADI Server Test Suite'
     description 'PACIO Advance Directives Server Test Suite'
@@ -16,6 +21,25 @@ module PacioTestKit
 
       exclude_message do |message|
         message.message.match?(/\A\S+: \S+: URL value '.*' does not resolve/)
+      end
+    end
+
+    config(
+      options: {
+        ig: 'ADI',
+        capability_statement_url: 'http://hl7.org/fhir/us/pacio-adi/CapabilityStatement/adi',
+        supported_resources: ADI_RESOURCES.keys,
+        required_profiles: ADI_RESOURCES.values.flatten
+      }
+    )
+
+    group do
+      title 'ADI FHIR API'
+
+      group from: :capability_statement do
+        description ERB.new(File.read(
+                              File.expand_path('../docs/capability_statement_group_description.md.erb', __dir__)
+                            )).result_with_hash(config:)
       end
     end
   end
