30b118fcbffa6d58a832809d2bffba2c10b999ee
Yunwei Wang <>
2025-10-13 08:38:16 -0500

    remove delete test from error_handling test group

--- lib/pacio_test_kit/custom_groups/error_handling_group.rb
+++ lib/pacio_test_kit/custom_groups/error_handling_group.rb
@@ -1,6 +1,5 @@
 require_relative 'error_tests/invalid_parameter_error_test'
 require_relative 'error_tests/unknown_resource_error_test'
-require_relative 'error_tests/deleted_resource_error_test'
 require_relative 'error_tests/unauthorized_request_error_test'
 
 module PacioTestKit
@@ -9,20 +8,17 @@ module PacioTestKit
     id :pacio_error_handling
     description %(
       This test group verifies that a FHIR server conformant to this IG can properly respond to malformed requests.
-      It includes tests for handling invalid parameters, unknown resources, deleted resources,
-      and unauthorized requests.
+      It includes tests for handling invalid parameters, unknown resources, and unauthorized requests.
 
       Per the FHIR specification, this group of tests ensures that the server returns the following responses classes:
         - (Status 400): invalid parameter
         - (Status 401/4xx): unauthorized request
         - (Status 404): unknown resource
-        - (Status 410): deleted resource.
     )
     input_order :url
 
     test from: :pacio_invalid_parameter_error
     test from: :pacio_unknown_resource_error
-    test from: :pacio_deleted_resource_error
     test from: :pacio_unauthorized_request_error
   end
 end
--- lib/pacio_test_kit/custom_groups/error_tests/deleted_resource_error_test.rb
+++ lib/pacio_test_kit/custom_groups/error_tests/deleted_resource_error_test.rb
@@ -12,6 +12,10 @@ module PacioTestKit
     input :deleted_resource_type,
           title: 'Resource type for the deleted resource from the server for deleted resource error testing'
 
+    # This test does not align with the FHIR Specification (http://hl7.org/fhir/http.html#delete)
+    # “For security reasons, the server may return other status codes as defined under Access Denied Response Handling.”
+    # This test has been removed from the PACIO Error Test Group.
+    # It can be added manually to a specific test suite if required.
     run do
       skip_if deleted_resource_id.blank? || deleted_resource_type.blank?,
               'No deleted resource ID or resource type provided'
